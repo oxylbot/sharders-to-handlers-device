455cfb423b32485d5b44d082e3b4fdcd40eb1a54
Joseph Banks <joseph@josephbanks.me>
2018-12-30 23:09:02 +0000

    Make it work

--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"github.com/zeromq/goczmq"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
-func main() {
+func getAddresses() (string, string) {
 	shardersBind := os.Getenv("PUSH_ADDRESS")
 	handlersBind := os.Getenv("PULL_ADDRESS")
+
+	if shardersBind == "" || handlersBind == "" {
+		panic("PUSH_ADDRESS and PULL_ADDRESS are not both set, please check them")
+	}
+
+	return shardersBind, handlersBind;
+}
+
+func main() {
+	shardersBind, handlersBind := getAddresses();
 
 	verboseLogging := os.Getenv("PROXY_DEBUG")
 
@@ -16,8 +28,6 @@ func main() {
 	if verboseLogging == "1" {
 		proxy.Verbose()
 	}
-
-	defer proxy.Destroy()
 
 	err := proxy.SetBackend(goczmq.Push, shardersBind)
 	if err != nil {
@@ -30,4 +40,17 @@ func main() {
 		proxy.Destroy()
 		panic(err)
 	}
+
+	signalChannel := makeSignalChannel()
+	<- signalChannel
+
+	proxy.Destroy()
+}
+
+func makeSignalChannel() (chan os.Signal) {
+	signalChannel := make(chan os.Signal, 1)
+
+	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	return signalChannel
 }
